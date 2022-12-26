4fb8224f6d9966e84890fc3a078fecdf15c65f10
dannyntale10 <dannyntale10@gmail.com>
2022-12-25 23:37:49 -0800

    fixed

--- factorization.py
+++ factorization.py
@@ -1,5 +1,4 @@
 #/usr/bin/env python3
-
 import sys
 
 
@@ -17,5 +16,6 @@ def primef(n):
             if n % (i + 2) == 0:
                 return primef(n/(i+2))
     return int(n)
+
 
 print(primef(int(sys.argv[1])))
